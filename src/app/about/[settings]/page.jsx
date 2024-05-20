462da4ec9bdb2d12a1b7b3a0fb2bfe3cc9e8ca0e
sethosa <osakonoseth1@gmail.com>
2024-05-20 11:12:59 +0000

    this is my testcommit

---
+++ src/app/about/[settings]/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Settings({param}) {
+    const paramx = param
+    console.log(paramx)
+  return (
+    <div>
+      I am settings
+    </div>
+  )
+}
+
+export default Settings
